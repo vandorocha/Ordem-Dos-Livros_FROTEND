bd8735eca941e8fbf198afb610d8af71d63985d6
vandorocha <vandorcdev@gmail.com>
2024-12-03 22:26:23 -0300

    Criado Rodapé

--- src/components/template/Pagina.svelte
+++ src/components/template/Pagina.svelte
@@ -1,14 +1,16 @@
 <script>
     import Cabecalho from "./Cabecalho.svelte";
 import Logo from "./Logo.svelte";
+    import Rodape from "./Rodape.svelte";
 
 </script>
 <div class="flex flex-col min-h-screen bg-black text-white">
 
     <Cabecalho />
-    <main>
+    <main class="flex-1">
         <slot>
 
         </slot>
     </main>
+    <Rodape />
 </div>
